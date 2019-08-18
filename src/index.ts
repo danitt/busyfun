0cd767cfb44a79a26a0f4a18564ca4f29bf52dce
Dan <dan.pelichowski@tabcorp.com.au>
2019-08-19 09:04:26 +1000

    testing other colour ranges

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Busylight from 'busylight';
 import * as Rainbow from 'color-rainbow';
+import * as tinygradient from 'tinygradient';
 
 // IIFE
 (async (): Promise<void> => {
@@ -11,9 +12,15 @@ import * as Rainbow from 'color-rainbow';
   // ..pulse
   // busylight.pulse();
 
-  // ..colour cycle
-  const colours = Rainbow.create(1000);
-  const coloursHex: string[] = colours.map((c: any): string => c.hexString());
+  // ..rainbow colour cycle
+  // const colours = Rainbow.create(1000);
+  // const coloursHex: string[] = colours.map((c: any): string => c.hexString());
+  // busylight.blink(coloursHex, 20);
+
+  // ..cycle custom colour gradient
+  const gradient = tinygradient('purple', 'orange', 'purple', 'orange', 'pink');
+  const coloursRgb = gradient.rgb(1000);
+  const coloursHex: string[] = coloursRgb.map((c: any): string => c.toHexString());
   busylight.blink(coloursHex, 20);
 
   // process.exit();
